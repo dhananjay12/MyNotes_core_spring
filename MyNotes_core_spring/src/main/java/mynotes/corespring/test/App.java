b227ac567adf17c72ef02f4cfa541cb88cbe4597
dj <dhananjay1204@gmail.com>
2014-04-15 07:18:23 +0530

    After wrting core spring : 1 , injection

--- MyNotes_core_spring/src/main/java/mynotes/corespring/test/App.java
+++ MyNotes_core_spring/src/main/java/mynotes/corespring/test/App.java
@@ -8,7 +8,7 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
 		Employee employee=(Employee) context.getBean("employee");
-		employee.getDetails();
+		System.out.println(employee);
 	}
 
 }
--- MyNotes_core_spring/src/main/java/mynotes/corespring/test/Employee.java
+++ MyNotes_core_spring/src/main/java/mynotes/corespring/test/Employee.java
@@ -1,9 +1,57 @@
 package mynotes.corespring.test;
 
 public class Employee {
-	
-	public void getDetails(){
-		System.out.println("Hello! I am an Employee..");
+
+	private int id;
+	private String name;
+	private Address homeAddress;
+	private Address officeAddress;
+
+	public Employee() {
+
+	}
+
+	public Employee(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return ("Id=>" + this.id + " Name=>" + this.name + "\nhomeAddress=>"
+				+ this.homeAddress + "\nofficeAddress=>" + this.officeAddress);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getHomeAddress() {
+		return homeAddress;
+	}
+
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
 	}
 
 }
