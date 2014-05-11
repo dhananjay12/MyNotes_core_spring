52d3e00fa956a478b2577b6583d1dd430ae049a3
dj <dhananjay1204@gmail.com>
2014-05-11 21:48:21 +0530

    Commit for Core Spring : Inner Beans, Alias, Injecing List and Autowiring

--- MyNotes_core_spring/src/main/java/mynotes/corespring/test/Employee.java
+++ MyNotes_core_spring/src/main/java/mynotes/corespring/test/Employee.java
@@ -1,11 +1,15 @@
 package mynotes.corespring.test;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Employee {
 
 	private int id;
 	private String name;
 	private Address homeAddress;
 	private Address officeAddress;
+	private List<String> programmingLaguages;
 
 	public Employee() {
 
@@ -18,8 +22,19 @@ public class Employee {
 
 	@Override
 	public String toString() {
+		String allLanguages = null;
+		
+		for (String language : this.programmingLaguages) {
+			if(allLanguages==null){
+			allLanguages=language+";";
+			}else{
+			allLanguages=allLanguages+language+";";
+			}
+		}
+		
 		return ("Id=>" + this.id + " Name=>" + this.name + "\nhomeAddress=>"
-				+ this.homeAddress + "\nofficeAddress=>" + this.officeAddress);
+				+ this.homeAddress + "\nofficeAddress=>" + this.officeAddress
+				+ " \nAllLanguage=>"+allLanguages);
 	}
 
 	public int getId() {
@@ -52,6 +67,14 @@ public class Employee {
 
 	public void setOfficeAddress(Address officeAddress) {
 		this.officeAddress = officeAddress;
+	}
+
+	public List<String> getProgrammingLaguages() {
+		return programmingLaguages;
+	}
+
+	public void setProgrammingLaguages(List<String> programmingLaguages) {
+		this.programmingLaguages = programmingLaguages;
 	}
 
 }
